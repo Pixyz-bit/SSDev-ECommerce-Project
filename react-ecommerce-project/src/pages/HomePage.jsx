8f9187b662b6207e1e30638df877f87f5fa50a5d
Arlan Martin N. Jalop <kaiphoenix48@gmail.com>
2026-03-28 00:44:28 +0800

    Use data from backend to generate the HTML

--- react-ecommerce-project/src/pages/HomePage.jsx
+++ react-ecommerce-project/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ export function HomePage() {
     axios.get("http://localhost:3000/api/products").then((response) => {
       setProducts(response.data);
     });
-  },[]);
+  }, []);
 
   // fetch('http://localhost:3000/api/products')
   // .then((response)=>{
