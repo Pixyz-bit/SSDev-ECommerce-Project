8293f8dc4aa5efdd23557eb2b5361c130d0d258a
Arlan Martin N. Jalop <kaiphoenix48@gmail.com>
2026-04-02 00:54:53 +0800

    Lift cart ip and share it between pages

--- react-ecommerce-project/src/pages/HomePage.jsx
+++ react-ecommerce-project/src/pages/HomePage.jsx
@@ -4,20 +4,15 @@ import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 //import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 //import { products } from "../../starting-code/data/products";
-export function HomePage() {
+export function HomePage({cart}) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       //axios.get("http://localhost:3000/api/products") //check vite.config.js for server: proxy
       setProducts(response.data);
     });
-
-    axios.get("/api/cart-items").then((response) => {
-      setCart(response.data);
-    });
   }, []);
-
   // fetch('http://localhost:3000/api/products')
   // .then((response)=>{
   //     response.json().then((data)=>{
